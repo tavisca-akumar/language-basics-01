0d7a7964ebba2904a9a866f789a6ca3b5f9f6ac4
tavisca-akumar <51955179+tavisca-akumar@users.noreply.github.com>
2019-06-23 00:47:59 +0530

    Language-Basics#1

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,7 +22,119 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+            string a = "";
+            string b = "";
+            string c = "";
+            int x = 0, y = 0;
+            int flag = 0;
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (equation[i] == '*')
+                {
+                    break;
+                }
+                if (equation[i] == '?')
+                {
+                    flag = 1;
+                }
+                a = a + equation[i];
+            }
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (equation[i] == '*')
+                {
+                    x = i;
+                }
+                if (equation[i] == '=')
+                {
+                    y = i;
+                }
+            }
+
+            for (int i = x + 1; i < y; i++)
+            {
+                if (equation[i] == '?')
+                {
+                    flag = 2;
+                }
+                b = b + equation[i];
+            }
+            for (int i = y + 1; i < equation.Length; i++)
+            {
+                c = c + equation[i];
+                if (equation[i] == '?')
+                {
+                    flag = 3;
+                }
+            }
+            //System.out.println(a+" "+b+" "+c);
+            if (flag == 1)
+            {
+                int B = Int32.Parse(b);
+                int C = Int32.Parse(c);
+                int ans = C / B;
+                String aa = ans.ToString();
+
+                if (B * ans != C || (aa.Length != x))
+                {
+                    return -1;
+                }
+                else
+                {
+                    for (int i = 0; i < x; i++)
+                    {
+                        if (aa[i] != equation[i])
+                        {
+                            //System.out.println(aa.charAt(i));
+                            int m = (int)(aa[i] - '0');
+                            return m;
+                        }
+                    }
+                }
+            }
+            if (flag == 2)
+            {
+                int A = Int32.Parse(a);
+                int C = Int32.Parse(c);
+                int ans = C / A;
+                String aa = ans.ToString();
+
+                if (A * ans != C || (aa.Length != (y - x - 1)))
+                {
+                    return -1;
+                }
+                else
+                {
+                    for (int i = x + 1; i < y; i++)
+                    {
+                        if (aa[i - (x + 1)] != equation[i])
+                        {
+                            int m = (int)(aa[i - (x + 1)] - '0');
+                            return m;
+                        }
+                    }
+                }
+            }
+            if (flag == 3)
+            {
+                int A = Int32.Parse(a);
+                int B = Int32.Parse(b);
+                int ans = A * B;
+                String aa = ans.ToString();
+
+
+                for (int i = y + 1; i < equation.Length; i++)
+                {
+                    if (aa[i - (y + 1)] != equation[i])
+                    {
+                        int m = (int)(aa[i - (y + 1)] - '0');
+                        return m;
+                    }
+                }
+            }
+            return -1;
             throw new NotImplementedException();
         }
     }
